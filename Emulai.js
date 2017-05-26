b16720fbea38c251abe935f6495c90bf0d0dcb1f
Herofied1180 <noahsturner06@gmail.com>
2017-05-26 14:28:09 -0400

    HTJSL 0.3
    What's New
    =========
    - Bug Fixes :0
    =========

--- Emulai.js
+++ Emulai.js
@@ -122,5 +122,108 @@ var lib = {
     delete: function(item) {
       lib.localStorage.removeItem(item);
     },
+    
+    boot: function (mode){
+      var pjs = lib.pI;
+      
+      if(lib.localStorage.getItem("booted") === "true"){
+                      if(mode === "boot") {
+        pjs.background(159, 210, 252);
+        pjs.fill(10 - 159, 210, 22 - 255 - 252);
+        pjs.textSize(25);
+        pjs.text("Di OS\n   ♨️", 425 - 75 * 2, 250);
+
+        var DiOScriptLogPos = {
+    
+            xPos: 175,
+            yPos: 200
+    
+        };
+    
+            if (pjs.millis() > 5000){
+        
+                    pjs.background(159, 210, 252);
+                    pjs.fill(69, 171, 255);
+                    pjs.stroke(28, 155, 252);
+                    pjs.rect(-1, 600 - 151, 602, 50);
+                    pjs.textSize(35);
+                    pjs.text("♨️", 560, 485);
+                    pjs.fill(204, 108, 108);
+                    pjs.text("     Desktop\nComing Soon...", 200, 250);
+                    
+                
+    
+            
+            }
+                      }
+      } else if(lib.localStorage.getItem("booted") !== "true") {
+              if(mode === "boot") {
+        pjs.background(159, 210, 252);
+        pjs.fill(10 - 159, 210, 22 - 255 - 252);
+        pjs.textSize(25);
+        pjs.text("Di OS\n   ♨️", 425 - 75 * 2, 250);
+
+        var DiOScriptLogPos = {
+    
+            xPos: 175,
+            yPos: 200
+    
+        };
+
+        pjs.draw  = function() {
+    
+            if (pjs.millis() > 5000){
+        
+                pjs.background(159, 210, 252);
+                pjs.fill(247, 247, 247);
+                pjs.noStroke();
+                pjs.rect(60, 135, 295, 120);
+                pjs.textSize(70);
+                pjs.text("🛑", 65, 215);
+                pjs.fill(36, 36, 36);
+                pjs.textSize(15);
+                pjs.text("Di OS would like permission to \noverwrite the current OS.\nIs this OK?", 140, 165);
+                pjs.fill(73, 170, 250);
+                pjs.rect(255, 210, 80, 30);
+                pjs.fill(230, 230, 230);
+                pjs.rect(150, 210, 80, 30);
+                pjs.fill(77, 77, 77);
+                pjs.text("OK", 285, 230);
+                pjs.text("Cancel", 165, 230);
+                pjs.draw = function() {
+        
+                    pjs.mouseClicked = function(){
+                if (pjs.mouseX > 150 && pjs.mouseX < 150 + 80 && pjs.mouseY > 210 && pjs.mouseY < 210 + 30){
+            
+                    pjs.background(0, 9, 255);
+                    for (var i = 0; i < 300; i++){
+                    var y = i * 4;
+                    pjs.fill(255, 255, 255);
+                    pjs.text(pjs.random(pjs.Number.MAX_SAFE_INTEGER, pjs.Number.MAX_SAFE_INTEGER * 9182034712) + "#d%ja3" + pjs.random(pjs.random(0, pjs.Number.MAX_SAFE_INTEGER), pjs.random(9, 8023)) + "#$201m#3291v!*" + pjs.random(0, pjs.Number.MAX_SAFE_INTEGER + 99999) + "(w;j98h&*(h38t7HUIh807&*(h837h9g(*&OHUIr" + pjs.random(2, pjs.Number.MAX_SAFE_INTEGER) + "7goth324;u4th82753oq34@\n%3uiphger;shugsoih", pjs.random(27 * pjs.random(0, 50) - pjs.random(0, 23), y / pjs.random(50, 850)), y);
+                    }
+            
+                } else if (pjs.mouseX > 255 && pjs.mouseX < 255 + 80 && pjs.mouseY > 210 && pjs.mouseY < 210 + 30){
+                    
+                    pjs.background(159, 210, 252);
+                    pjs.fill(69, 171, 255);
+                    pjs.stroke(28, 155, 252);
+                    pjs.rect(-1, 600 - 151, 602, 50);
+                    pjs.textSize(35);
+                    pjs.text("♨️", 560, 485);
+                    pjs.fill(204, 108, 108);
+                    pjs.text("     Desktop\nComing Soon...", 200, 250);
+                    lib.localStorage.setItem("booted")
+                    
+                }
+        
+            };
+    
+            };
+            }
+    
+        };
+              }
+      }
+    },
   }
 };
